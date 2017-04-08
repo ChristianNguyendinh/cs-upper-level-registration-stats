0b6d594876b3c338d7a7eb1851c1d87efca108e5
Christian <christiannguyendinh@yahoo.com>
2017-04-08 19:12:11 -0400

    update port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var server = express();
 
+server.set('port', (process.env.PORT || 8000));
+
 server.get('/', function(req, res) {
 	res.send("Made it")
 })
 
-server.listen(8080, function() {
-	console.log('Server running on port 8080');
+server.listen(server.get('port'), function() {
+	console.log('Server running on port 8000');
 });
