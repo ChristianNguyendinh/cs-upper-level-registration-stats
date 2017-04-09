940857ad20d25b87b957c71dc86fc119a90bf738
Christian <christiannguyendinh@yahoo.com>
2017-04-08 20:15:57 -0400

    node calling python scripts

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var server = express();
+var CronJob = require('cron').CronJob;
+var PythonShell = require('python-shell');
+
+var job = new CronJob({
+	cronTime: '*/15 * * * * *',
+	onTick: function() {
+		console.log("tick tok");
+		// for now use the python script until someone writes it in node
+		PythonShell.run('manual/scrape_to_test.py', function (err) {
+			if (err) throw err;
+			console.log("done");
+		}).on('message', function (message) {
+			console.log(message);
+		});;
+	},
+	start: false,
+	timeZone: 'America/New_York'
+});
 
 server.set('port', (process.env.PORT || 8000));
 
@@ -8,5 +26,6 @@ server.get('/', function(req, res) {
 })
 
 server.listen(server.get('port'), function() {
-	console.log('Server running on port 8000');
+	job.start();
+	console.log('Server running on port ' + server.get('port'));
 });
