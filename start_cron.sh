89e7326f1ae8e0c9409c05b0127455489dedd603
Christian <christiannguyendinh@yahoo.com>
2017-09-10 23:15:20 -0400

    script to start up the cron job

---
+++ start_cron.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get full directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+crontab -l > tempcron
+echo "30 06 * * * node ${DIR}/scrape_data.js >> ${DIR}/logs/scrape-debug.log" >> tempcron
+crontab tempcron
+rm tempcron
